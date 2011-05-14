e3a981453abb08d1c206649ad9c485e2d1116d06
Rob Graham <github@mellowplace.com>
2011-05-14 21:13:02 +0100

    Updated submodule

--- lib/form/minacl/base/BaseFirmForm.class.php
+++ lib/form/minacl/base/BaseFirmForm.class.php
@@ -21,15 +21,21 @@ abstract class BaseFirmForm extends BaseFormMinaclPropel
 		/*
 		 * Validators for the ID column
 		 */
-		$id1 = new phNumericValidator();
-		$id1->decimal(false)->min(-2147483648)->max(2147483647);
-		$this->id->setValidator($id1);
+		if($this->getView()->hasData('id'))
+		{
+			$id1 = new phNumericValidator();
+			$id1->decimal(false)->min(-2147483648)->max(2147483647);
+			$this->id->setValidator($id1);
+		}
 		/*
 		 * Validators for the NAME column
 		 */
-		$name1 = new phStringLengthValidator();
-		$name1->max(50);
-		$this->name->setValidator($name1);
+		if($this->getView()->hasData('name'))
+		{
+			$name1 = new phStringLengthValidator();
+			$name1->max(50);
+			$this->name->setValidator($name1);
+		}
 	}
 
 	public function getModelName()
--- lib/form/minacl/base/BaseUserForm.class.php
+++ lib/form/minacl/base/BaseUserForm.class.php
@@ -25,39 +25,54 @@ abstract class BaseUserForm extends BaseFormMinaclPropel
 		/*
 		 * Validators for the ID column
 		 */
-		$id1 = new phNumericValidator();
-		$id1->decimal(false)->min(-2147483648)->max(2147483647);
-		$this->id->setValidator($id1);
+		if($this->getView()->hasData('id'))
+		{
+			$id1 = new phNumericValidator();
+			$id1->decimal(false)->min(-2147483648)->max(2147483647);
+			$this->id->setValidator($id1);
+		}
 		/*
 		 * Validators for the FIRM_ID column
 		 */
-		$firm_id1 = new phNumericValidator();
-		$firm_id1->decimal(false)->min(-2147483648)->max(2147483647);
-		$firm_id2 = new phRequiredValidator();
-		$firm_id3 = new sfMinaclPropelChoiceValidator('Firm');
-		$firm_id3->setMultiple(false);
-		$this->firm_id->setValidator(new phValidatorLogic($firm_id1))->
-			and_($firm_id2)->		
-			and_($firm_id3);		
+		if($this->getView()->hasData('firm_id'))
+		{
+			$firm_id1 = new phNumericValidator();
+			$firm_id1->decimal(false)->min(-2147483648)->max(2147483647);
+			$firm_id2 = new phRequiredValidator();
+			$firm_id3 = new sfMinaclPropelChoiceValidator('Firm');
+			$firm_id3->setMultiple(false);
+			$this->firm_id->setValidator(new phValidatorLogic($firm_id1))->
+				and_($firm_id2)->		
+				and_($firm_id3);		
+		}
 		/*
 		 * Validators for the NAME column
 		 */
-		$name1 = new phStringLengthValidator();
-		$name1->max(50);
-		$name2 = new phRequiredValidator();
-		$this->name->setValidator(new phValidatorLogic($name1))->
-			and_($name2);		
+		if($this->getView()->hasData('name'))
+		{
+			$name1 = new phStringLengthValidator();
+			$name1->max(50);
+			$name2 = new phRequiredValidator();
+			$this->name->setValidator(new phValidatorLogic($name1))->
+				and_($name2);		
+		}
 		/*
 		 * Validators for the PROFILE column
 		 */
-		$profile1 = new phStringLengthValidator();
-		$this->profile->setValidator($profile1);
+		if($this->getView()->hasData('profile'))
+		{
+			$profile1 = new phStringLengthValidator();
+			$this->profile->setValidator($profile1);
+		}
 		/*
 		 * Validators for the UserInterest many 2 many table
 		 */
-		$user_interest_list = new sfMinaclPropelChoiceValidator('Interest');
-		$user_interest_list->setMultiple(true);
-		$this->user_interest_list->setValidator($user_interest_list);
+		if($this->getView()->hasData('user_interest_list'))
+		{
+			$user_interest_list = new sfMinaclPropelChoiceValidator('Interest');
+			$user_interest_list->setMultiple(true);
+			$this->user_interest_list->setValidator($user_interest_list);
+		}
 	}
 
 	public function getModelName()
